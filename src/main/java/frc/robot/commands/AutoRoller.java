568355624a7a3a30737fd4258ae1f8e0018ee736
J0C0I <josh.sengmin@gmail.com>
2025-01-23 20:19:04 -0800

    gybyugby

---
+++ src/main/java/frc/robot/commands/AutoRoller.java
@@ -0,0 +1,32 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CANRollerSubsystem;
+
+public class AutoRoller extends Command {
+    private final CANRollerSubsystem rollerSubsystem;
+
+    public AutoRoller(CANRollerSubsystem rollerSubsystem) {
+        this.rollerSubsystem = rollerSubsystem;
+        addRequirements(rollerSubsystem);
+    }
+    @Override
+    public void initialize() {
+        System.out.println("started");
+    }
+    @Override
+    public void execute() {
+        rollerSubsystem.setRollerMotor(0.5);
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        rollerSubsystem.setRollerMotor(0.0);
+        System.out.println("ended");
+    }
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
