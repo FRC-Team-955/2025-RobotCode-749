d5d282b508535d3daa0c20c2c5698e1ce37b9db2
Team 955 <captain@cvhsrobotics.com>
2025-01-31 20:22:04 -0800

    pidauto

--- src/main/java/frc/robot/commands/AutoForward.java
+++ src/main/java/frc/robot/commands/AutoForward.java
@@ -9,42 +9,46 @@ public class AutoForward extends Command {
     private final CANDriveSubsystem driveSubsystem;
     private final double distance;
     private double encoderSetpoint;
-    PIDController pid = new PIDController(0.002, 0, 0);
-    private double speed;
-    private double slowSpeed;
+    private final PIDController pid;
+    //private double speed;
+    //private double slowSpeed;
 
     public AutoForward(CANDriveSubsystem driveSubsystem, double distance) {
         this.driveSubsystem = driveSubsystem;
         this.distance = distance;
+        this.pid = new PIDController(0.01, 0, 0);
         addRequirements(driveSubsystem);
     }
 
 
     @Override
     public void initialize() {
+        //pid.setTolerance(5, 10);
         System.out.println("started");
-        pid.reset();
-        pid.setTolerance(5, 10);
         encoderSetpoint = driveSubsystem.currentDistance() + distance;
-        speed = (pid.calculate(driveSubsystem.currentDistance(), encoderSetpoint));
+        pid.setSetpoint(encoderSetpoint);
+        //speed = (pid.calculate(driveSubsystem.currentDistance(), encoderSetpoint));
+        //slowSpeed = (pid.calculate(driveSubsystem.currentDistance(), encoderSetpoint));
     }
 
     @Override
     public void execute() {
+        //pid.setSetpoint(encoderSetpoint);
+        double speed = (pid.calculate(driveSubsystem.currentDistance(), encoderSetpoint));
         driveSubsystem.setSpeed(speed, speed);
+        System.out.println("started");
     }
 
     @Override
     public void end(boolean interrupted) {
-        slowSpeed = (pid.calculate(driveSubsystem.currentDistance(), encoderSetpoint));
-        driveSubsystem.setSpeed(slowSpeed, slowSpeed);
-        //pid.atSetpoint();
+        //double slowSpeed = (pid.calculate(driveSubsystem.currentDistance(), encoderSetpoint));
+        driveSubsystem.setSpeed(0, 0);
         System.out.println("ended");
     }
 
     @Override
     public boolean isFinished() {
-        if (pid.atSetpoint()) {
+        if (driveSubsystem.currentDistance() > encoderSetpoint) {
             return true;
         }
         else
