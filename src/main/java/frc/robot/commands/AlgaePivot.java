c2eaed11d9ee42a820b655669a3649c3b79cb5bf
markyeh1 <www.author.yeh@gmail.com>
2025-02-20 16:50:52 -0800

    EleAVTOR code updated

--- src/main/java/frc/robot/commands/AlgaePivot.java
+++ src/main/java/frc/robot/commands/AlgaePivot.java
@@ -1,54 +1,56 @@
-//package frc.robot.commands;
-//
-//import edu.wpi.first.math.controller.PIDController;
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-//import edu.wpi.first.wpilibj2.command.Command;
-//import frc.robot.Constants;
-//import frc.robot.subsystems.AlgaeSubSystem;
-//import frc.robot.subsystems.CANRollerSubsystem;
-//
-//public class AlgaePivot extends Command {
-//    private final AlgaeSubSystem algaeSubSystem;
-//    private double encoderSetpoint;
-//    private final PIDController pidController;
-//    private double speed;
-//    private final CANRollerSubsystem rollerSubsystem;
-//    private double inOrOut;
-//
-//
-//    public AlgaePivot(AlgaeSubSystem algaeSubSystem, CANRollerSubsystem rollerSubsystem, double inOrOut, double targetDistance) {
-//        this.algaeSubSystem = algaeSubSystem;
-//        this.pidController = new PIDController(0.001, 0, 0);
-//        this.encoderSetpoint = targetDistance;
-//        addRequirements(algaeSubSystem);
-//        this.rollerSubsystem = rollerSubsystem;
-//        this.inOrOut = inOrOut;
-//        addRequirements(rollerSubsystem);
-//    }
-//
-//    @Override
-//    public void initialize() {
-//        pidController.reset();
-//        SmartDashboard.putNumber("algaeEncoderSetpoint", encoderSetpoint);
-//    }
-//
-//    @Override
-//    public void execute() {
-//        this.speed = pidController.calculate(algaeSubSystem.currentAlgaePivotEncoder(), encoderSetpoint);
-//        algaeSubSystem.setSpeed(speed);
-//        rollerSubsystem.setAlgaeRollerMotor(inOrOut);
-//        SmartDashboard.putNumber("AlgaePivotPidOutput", speed);
-//
-//    }
-//
-//    @Override
-//    public void end(boolean interrupted) {
-//        algaeSubSystem.setSpeed(speed);
-//        rollerSubsystem.setAlgaeRollerMotor(0.0);
-//    }
-//
-//    @Override
-//    public boolean isFinished() {
-//        return false;
-//    }
-//}
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.AlgaeSubSystem;
+import frc.robot.subsystems.CANRollerSubsystem;
+
+public class AlgaePivot extends Command {
+    private final AlgaeSubSystem algaeSubSystem;
+    private double encoderSetpoint;
+    private final PIDController pidController;
+    private double speed;
+    private final CANRollerSubsystem rollerSubsystem;
+    private double inOrOut;
+
+
+    public AlgaePivot(AlgaeSubSystem algaeSubSystem, CANRollerSubsystem rollerSubsystem, double inOrOut, double targetDistance) {
+        this.algaeSubSystem = algaeSubSystem;
+        this.pidController = new PIDController(0.000001, 0, 0);
+        this.encoderSetpoint = targetDistance;
+        addRequirements(algaeSubSystem);
+        this.rollerSubsystem = rollerSubsystem;
+        this.inOrOut = inOrOut;
+        addRequirements(rollerSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+        pidController.reset();
+        SmartDashboard.putNumber("algaeEncoderSetpoint", encoderSetpoint);
+        SmartDashboard.putNumber("AlgetPivotPosition", encoderSetpoint);
+    }
+
+    @Override
+    public void execute() {
+        //this.speed = pidController.calculate(algaeSubSystem.currentAlgaePivotEncoder(), encoderSetpoint);
+        this.speed = pidController.calculate(algaeSubSystem.currentEncoderPivotPosition(), encoderSetpoint);
+        algaeSubSystem.setSpeed(speed);
+        rollerSubsystem.setAlgaeRollerMotor(inOrOut);
+        SmartDashboard.putNumber("AlgaePivotPidOutput", speed);
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        algaeSubSystem.setSpeed(speed);
+        rollerSubsystem.setAlgaeRollerMotor(0.0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
--- src/main/java/frc/robot/subsystems/AlgaeSubSystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeSubSystem.java
@@ -1,39 +1,47 @@
-//package frc.robot.subsystems;
-//
-//import com.revrobotics.AbsoluteEncoder;
-//import com.revrobotics.RelativeEncoder;
-//import com.revrobotics.spark.SparkBase;
-//import com.revrobotics.spark.SparkLowLevel;
-//import com.revrobotics.spark.SparkMax;
-//import com.revrobotics.spark.config.SparkBaseConfig;
-//import com.revrobotics.spark.config.SparkMaxConfig;
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-//import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//import frc.robot.Constants;
-//
-//public class AlgaeSubSystem extends SubsystemBase {
-//    private final SparkMax algaePivot;
-//    private final AbsoluteEncoder algaePivotEncoder;
-//
-//    public AlgaeSubSystem() {
-//       algaePivot = new SparkMax(Constants.AlgaeConstants.Algae_Intake_ID, SparkLowLevel.MotorType.kBrushless);
-//       algaePivotEncoder = algaePivot.getAbsoluteEncoder();
-//        SparkBaseConfig config = new SparkMaxConfig();
-//        config.idleMode(SparkBaseConfig.IdleMode.kBrake);
-//        algaePivot.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-//    }
-//
-//
-//    public void setSpeed(double speed) {
-//        algaePivot.set(speed);
-//    }
-//
-//
-//    @Override
-//    public void periodic() {
-//        SmartDashboard.putNumber("currentAlgaePivotEncoder", currentAlgaePivotEncoder());
-//    }
-//    public double currentAlgaePivotEncoder() {
-//        return algaePivotEncoder.getPosition();
-//    }
-//}
+package frc.robot.subsystems;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class AlgaeSubSystem extends SubsystemBase {
+    private final SparkMax algaePivot;
+    private final AbsoluteEncoder algaePivotEncoder;
+    private final DutyCycleEncoder encoder;
+
+    public AlgaeSubSystem() {
+       algaePivot = new SparkMax(Constants.AlgaeConstants.Algae_Intake_ID, SparkLowLevel.MotorType.kBrushless);
+       algaePivotEncoder = algaePivot.getAbsoluteEncoder();
+       encoder = new DutyCycleEncoder(2);
+        SparkBaseConfig config = new SparkMaxConfig();
+        config.idleMode(SparkBaseConfig.IdleMode.kBrake);
+        algaePivot.configure(config, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+    }
+
+
+    public void setSpeed(double speed) {
+        algaePivot.set(speed);
+    }
+
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("currentAlgaePivotEncoder", currentAlgaePivotEncoder());
+        SmartDashboard.putNumber("Encoder", currentEncoderPivotPosition());
+    }
+    public double currentEncoderPivotPosition() {
+        return encoder.get();
+    }
+
+    public double currentAlgaePivotEncoder() {
+        return algaePivotEncoder.getPosition();
+    }
+}
